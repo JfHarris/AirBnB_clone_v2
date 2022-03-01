7e315036f9f7f1878ecffeddb0486f6ba69c3ac4
jfharris <jfharris1983@gmail.com>
2022-03-01 00:56:01 -0600

    Added if loop

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from os import getenv
+
+storage_comm = getenv('HBNB_TYPE_STORAGE')
 
 
-storage = FileStorage()
+if storage_comm == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage
+else:
+    storage = FileStorage()
 storage.reload()
+
+# Add things for DBStorage here---task 6
