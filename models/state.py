cfdba9342f08c2ef8493dc0018ec66f0e35f1604
jfharris <jfharris1983@gmail.com>
2022-03-01 01:03:55 -0600

    Added getter and new attrs

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+import models
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.city import City
+storage_comm = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if storage_comm == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            city_inst = []
+            all_cities = models.storage.all(City)
+            for city in city_inst.values():
+                if city.state_id == self.id:
+                    city_inst.append(city)
+            return city_inst
