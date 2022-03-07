5b8000bae32e54c1b65459cc25efa40eb53c3a89
jfharris <jfharris1983@gmail.com>
2022-03-06 22:17:19 -0600

    Added test db storage

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Testing db storage
+"""
+import unittest
+import os
+from models import storage
+from models.base_model import BaseModel
+
+
+class test_dbstorage(unittest. TestCase):
+    """
+    Testing db storage
+    """
+
+    def test_obj_list_empty(self):
+        """ __objects is initially empty """
+        self.assertNotEqual(len(storage.all()), 0)
+
+    def test_all(self):
+        """
+        test all method
+        """
+        new = BaseModel()
+        holder = storage.all()
+        self.assertIsInstance(holder, dict)
+
+    def test_new(self):
+        """ New object is correctly added to __objects """
+        new = BaseModel()
+        new.save()
+        for obj in storage.all().values():
+            temp = obj
+        self.assertTrue(temp is obj)
