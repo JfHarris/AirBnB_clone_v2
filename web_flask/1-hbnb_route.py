a0648982f4287ba71e795cf765fe1952ac4ef43f
jfharris <jfharris1983@gmail.com>
2022-03-17 17:31:21 -0500

    Added /hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,9 +10,17 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def greet():
     """
-    returns Hello HBNB!
+    display “Hello HBNB!”
     """
     return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    display HBNB
+    """
+    return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
