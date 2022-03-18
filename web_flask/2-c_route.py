a40fe3e3b553636b79f0b6333023f80dbd9d37d5
jfharris <jfharris1983@gmail.com>
2022-03-17 22:14:08 -0500

    Added space after C

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,9 +10,24 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def greet():
     """
-    returns Hello HBNB!
+    display “Hello HBNB!”
     """
     return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    display HBNB
+    """
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """
+    displays “C ” followed by the value of the text
+    """
+    return 'C ' + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
