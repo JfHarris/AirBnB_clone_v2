644b1ecef44fdc87a5a93a1079a33ecd890c600e
jfharris <jfharris1983@gmail.com>
2022-03-19 14:52:11 -0500

    Edited var in H1

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -4,6 +4,7 @@ starts a Flask web application
 """
 
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
@@ -42,6 +43,25 @@ def number(n):
     display “n is a number” only if n is an integer
     """
     return "{:d} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numbertemplate(n):
+    """
+    display a HTML page only if n is an integer
+    """
+    return render_template('5-number.html', n=n)
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def numbersoORe(n):
+    """
+    display a HTML page only if n is an integer
+    """
+    if n % 2 == 0:
+        result = 'even'
+    else:
+        result = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n,
+                           result=result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
