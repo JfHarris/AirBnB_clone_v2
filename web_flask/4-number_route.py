aafa3157b130e5cf9d3aa83e60a0de1c9d6cd539
jfharris <jfharris1983@gmail.com>
2022-03-18 22:48:51 -0500

    Pep8

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -14,6 +14,7 @@ def greet():
     """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
@@ -21,12 +22,14 @@ def hbnb():
     """
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """
     displays “C ” followed by the value of the text
     """
     return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -36,12 +39,14 @@ def pythoniscool(text='is cool'):
     """
     return 'Python ' + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """
     display “n is a number” only if n is an integer
     """
     return "{:d} is a number".format(n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
